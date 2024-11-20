bf75e7c9fb4c4a5ce010a358f7777d04ec4c1c6d
ImAlexandr <4226175@gmail.com>
2024-11-20 19:04:42 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+# "Нули ничто, отрицание недопустимо!":
+
+i = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+while i < len(my_list):
+    if my_list[i]<0:
+        break
+    print(my_list[i])
+    i += 1
+    continue
+
