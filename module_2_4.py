ebe1d3b75cfc90150f68bdbf63e002da6692549f
ImAlexandr <4226175@gmail.com>
2024-11-26 01:14:52 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -5,19 +5,23 @@ primes = list()
 not_primes = list()
 
 for i in range(len(numbers)):
+    a=numbers[i]
     is_primes = True
     if numbers[i] <= 1:
-        is_primes = True
+        continue
     if numbers[i] == 2:
-        is_primes = False
+        pass
+
     for j in (range(2, numbers[i])):
-        is_primes = numbers[i] % j == 0
-        break
+        if numbers[i] % j == 0:
+            is_primes = False
+            break
 
     if is_primes is True:
-        not_primes.append(numbers[i])
-    else:
         primes.append(numbers[i])
+    else:
+        not_primes.append(numbers[i])
 
+print('Numbers',numbers)
 print('Primes',primes)
 print('Not_primes',not_primes)
